28c50ccd13b09bc3c0291dc44c75cccc1545a045
mohamedosama2074 <iosengks74@gmail.com>
2020-04-10 01:14:12 +0200

    Initial Commit

---
+++ ReusableCustomPopup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReusableCustomPopup
+//
+//  Created by Mohamed on 4/10/20.
+//  Copyright © 2020 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
